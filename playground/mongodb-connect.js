951ff4c98572e90381449ec19490f81e9ecde3b2
Anshul Reejonia <anshul.reejonia@gmail.com>
2018-09-06 17:41:38 +0530

    add delete file

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -9,26 +9,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server');
 
-    // db.collection('Todos').insertOne({
-    //     text: 'something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if(err){
-    //         return console.log('Unable to insert to mongo db server ' , err);
-    //     }
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
-
-    db.collection('Users').insertOne({
-        Name: 'Ruchi',
-        age: 29
+    db.collection('Todos').insertOne({
+        text: 'Eat Lunch',
+        completed: false
     }, (err, result) => {
         if(err){
             return console.log('Unable to insert to mongo db server ' , err);
         }
-        //console.log(JSON.stringify(result.ops, undefined, 2));
-        console.log(result.ops[0]._id.getTimestamp());
+        console.log(JSON.stringify(result.ops, undefined, 2));
     });
+
+    // db.collection('Users').insertOne({
+    //     Name: 'Ruchi',
+    //     age: 29
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to insert to mongo db server ' , err);
+    //     }
+    //     //console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
 
     db.close();
 });
