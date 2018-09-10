885c6c10cddc810218616dbb45405ca4be8183ce
Anshul Reejonia <anshul.reejonia@gmail.com>
2018-09-10 17:23:01 +0530

    mongoose queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,21 @@
+var {mongoose} = require('./../server/db/mongoose');
+var {Todo} = require('./../server/models/todo'); 
+
+var id = '5b9618046a865e2c40306b10';
+
+Todo.find({
+    _id: id
+}).then((doc) => {
+    console.log(doc);
+});
+
+
+Todo.findOne({
+    completed: false
+}).then((doc) => {
+    console.log(doc);
+});
+
+Todo.findById(id).then((doc) => {
+    console.log(doc);
+});
